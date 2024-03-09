7e7647df2303257bf746dee16f3100155b794b0a
thornbeam <thornbeam@posteo.eu>
2024-03-09 15:25:01 +0100

    feat(api): test_app - add bme280 api endpoint, getting json nan type error

--- django_feinstaub/urls.py
+++ django_feinstaub/urls.py
@@ -20,21 +20,30 @@ from django.urls import path
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from test_app.models import bme280
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
+class Bme280Serializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = bme280
+        fields = "__all__"
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+class Bme280ViewSet(viewsets.ModelViewSet):
+    queryset = bme280.objects.all()
+    serializer_class = Bme280Serializer
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'bme280s', Bme280ViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
