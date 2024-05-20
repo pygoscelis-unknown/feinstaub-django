f72872f53ba5c957776bd39e844db1730e7f3e66
thornbeam <thornbeam@posteo.eu>
2024-05-20 14:35:49 +0200

    feat(commands): test_app - init import data multiprocessing

---
+++ test_app/management/commands/modules/multiprocessing.py
@@ -0,0 +1,37 @@
+import multiprocessing
+from itertools import repeat
+from .create_object import create as create_object
+import datetime
+
+def main(sensor_type, header, rows):
+    with multiprocessing.Pool() as pool:
+        pool.starmap(func, zip(repeat(sensor_type), repeat(header), rows))
+
+def func(sensor_type, header, row):
+    # convert illegal values
+    for i in range(len(row)):
+        if header[i] != "sensor_type":
+            if header[i] == "sensor_id" or header[i] == "location":
+                #int
+                try:
+                    row[i] = int(row[i])
+                except ValueError:
+                    row[i] = None
+
+            elif header[i] == "timestamp":
+                #timestamp
+                try:
+                    row[i] = datetime.datetime.fromisoformat(row[i])
+                except ValueError:
+                    row[i] = None
+
+            else:
+                #float
+                try:
+                    row[i] = float(row[i])
+                except ValueError:
+                    row[i] = None
+
+    print(row)
+    create_object(sensor_type, row)
+    print("creating objects")
