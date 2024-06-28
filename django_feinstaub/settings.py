51cc0d9437915ee57e36f470a8f692f1c55f3d10
thornbeam <thornbeam@posteo.eu>
2024-06-28 20:19:58 +0200

    feat(chore): add pagination to api

--- django_feinstaub/settings.py
+++ django_feinstaub/settings.py
@@ -90,6 +90,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 
