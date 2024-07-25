ad3e0dbbf70ed7cf40c9f0d573f545a3b328b759
thornbeam <thornbeam@posteo.eu>
2024-07-26 00:41:59 +0200

    fix(commands): test_app/compare_sensor_types - rename to get_zip_sensor_types, change output format to json list, install prettier as dev dependency

--- test_app/management/commands/get_zip_sensor_types.py
+++ test_app/management/commands/get_zip_sensor_types.py
@@ -11,12 +11,12 @@ from .modules.sensor_type import get_sensor_type
 from .modules.create_object import create
 
 # command example
-# python manage.py compare_sensor_types --url http://archive.sensor.community 
+# python manage.py compare_sensor_types --url http://archive.sensor.community
 
 
 class Command(BaseCommand):
     help = """
-    Save sensor data of all monthly csv files: see http://archive.sensor.community/csv_per_month/
+    Print all sensor types saved as csv per month in zip format into json files: see http://archive.sensor.community/csv_per_month/
     """
 
     def add_arguments(self, parser):
@@ -30,15 +30,16 @@ class Command(BaseCommand):
         sensor_type_queue = dict()
 
         filenames = [
-            "list-zip_files.txt",
-            "list-sensor_types.txt",
+            "zip_filenames.json",
+            "sensor_types-zip.json",
         ]
         with open(filenames[0], "w") as pyf:
-            pyf.write("")
+            pyf.write("[\n")
         with open(filenames[1], "w") as pyf:
-            pyf.write("")
+            pyf.write("[\n")
 
         sensor_type_queue = []
+        index = 0
 
         for a in soup.find_all("a", href=True):
             href = a["href"]
@@ -55,16 +56,28 @@ class Command(BaseCommand):
                     if href in a["href"]:
                         sensor_type = get_sensor_type(a["href"], date, True)
                         with open(filenames[0], "a") as pyf:
-                            pyf.write(a["href"] + "\n")
+                            if index == 0:
+                                pyf.write('"' + a["href"] + '"')
+                            else:
+                                pyf.write(',\n"' + a["href"] + '"')
 
                         if sensor_type not in sensor_type_queue:
                             sensor_type_queue.append(sensor_type)
 
                             with open(filenames[1], "a") as pyf:
-                                pyf.write(sensor_type + "\n")
-                                print("register sensor type", sensor_type)
+                                if index == 0:
+                                    pyf.write('"' + sensor_type + '"')
+                                    index += 1
+                                else:
+                                    pyf.write(',\n"' + sensor_type + '"')
+                            print("register sensor type", sensor_type)
                         else:
                             print("sensor type already in queue, skip ...")
 
             except ValueError:
                 continue
+
+        with open(filenames[0], "a") as pyf:
+            pyf.write("\n]")
+        with open(filenames[1], "a") as pyf:
+            pyf.write("\n]")
