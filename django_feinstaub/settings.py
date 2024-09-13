fe998eed6d578e5b90a2d70a95726afe56ee9394
thornbeam <thornbeam@posteo.eu>
2024-09-13 08:59:33 +0200

    chore(project): change names of postgres env variables

--- django_feinstaub/settings.py
+++ django_feinstaub/settings.py
@@ -101,9 +101,9 @@ load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get("DB_ENGINE"),
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
     }
