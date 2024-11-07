0271add90b21716cd93f476af3f753d31901b6dc
thornbeam <thornbeam@posteo.eu>
2024-11-07 22:08:39 +0100

    build(project): rename project

--- feinstaub/wsgi.py
+++ feinstaub/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_feinstaub project.
+WSGI config for feinstaub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_feinstaub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feinstaub.settings')
 
 application = get_wsgi_application()
