3d43e150da546f00ea5ac683242462bc35310c63
thornbeam <114859938+thornbeam@users.noreply.github.com>
2024-11-07 23:07:30 +0100

    build: rename project and app, fixes #40 (#41)
    * build(project): rename project
    
    * build(app): rename test_app to importer

--- feinstaub/asgi.py
+++ feinstaub/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_feinstaub project.
+ASGI config for feinstaub project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_feinstaub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feinstaub.settings')
 
 application = get_asgi_application()
