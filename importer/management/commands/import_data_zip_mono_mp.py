d2886b9658c8316e9536677274d1c0b7af1de7d4
thornbeam <thornbeam@posteo.eu>
2024-12-18 00:26:53 +0100

    style(command): import_data-zip-mono-multiprocessing - pylint

--- importer/management/commands/import_data_zip_mono_mp.py
+++ importer/management/commands/import_data_zip_mono_mp.py
@@ -1,8 +1,14 @@
-from django.core.management import BaseCommand
+"""
+Imports data from a particular zip file available on:
+    `http://archive.sensor.community/csv_per_month/`
+Uses multiprocessing.
+"""
+
 import csv
 import urllib.request
 import zipfile
 import time
+from django.core.management import BaseCommand
 from .modules.multiprocessing import main as create_objects
 from .modules.get_env_vars import get_sensor_archive_url
 from .modules.csv import get_chunk, delete_sensor_data_files
@@ -10,9 +16,13 @@ from .modules.show_progress import show_download_progress
 
 
 class Command(BaseCommand):
+    """
+    Downloads a zip file of the specified sensor type, unzips them,
+    and loads data from the extracted csv file into database.
+    """
+
     help = """
-    Loads data from csv files into the database.
-    This command downloads zip files of a specific sensor type available in zip format, unzip them and load data from the extracted csv files into the database.
+    Loads data from a zipped csv file into database.
     """
 
     def add_arguments(self, parser):
@@ -25,9 +35,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
         base_url = get_sensor_archive_url()
-        year = kwargs["year"]
-        month = kwargs["month"]
-        date = year + "-" + month
+        date = kwargs["year"] + "-" + kwargs["month"]
         sensor_type = kwargs["type"]
         url = (
             base_url
@@ -50,7 +58,7 @@ class Command(BaseCommand):
             zip_ref.extractall("./")
 
         print("Opening file ...", end="\r")
-        with open(file_name + ".csv", newline="") as csvfile:
+        with open(file_name + ".csv", newline="", encoding="utf-8") as csvfile:
             print("Reading file ...", end="\r")
             reader = csv.reader(csvfile, delimiter=";")
 
@@ -59,7 +67,7 @@ class Command(BaseCommand):
             for index, chunk in get_chunk(reader, 100000):
                 if index == 0:
                     header = chunk.pop(0)
-                rows = [x for x in chunk]
+                rows = list(chunk)
 
                 if header:
                     create_objects(sensor_type, header, rows)
